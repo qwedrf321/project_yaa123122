4f4d9f174563f6fd6e3cd2a47f57b064b9b36e6d
qwedrf321 <nikitakadolba@gmail.com>
2026-02-13 20:43:29 +0200

    note_detail

--- notes/urls.py
+++ notes/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('list/', views.notes_list, name='notes_list'),
     path('create/', views.note_create, name='create'),
+    path("note/<int:pk>/", views.note_detail, name="detail"),
+    path('note/<int:pk>/delete/', views.delete_note, name='delete_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from .models import Note
 from .forms import NoteForm
-
 
 def index(request):
     return render(request, 'index.html')
 
-
 def notes_list(request):
-    notes = Note.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': notes})
+    notes_all = Note.objects.all().order_by('-created_at')
+    paginator = Paginator(notes_all, 3) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'notes/notes_list.html', {'page_obj': page_obj})
 
+def note_detail(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    return render(request, "notes/note_detail.html", {"note": note})
 
 @login_required
 def note_create(request):
@@ -25,3 +30,10 @@ def note_create(request):
     else:
         form = NoteForm(user=request.user)
     return render(request, 'notes/notes_create.html', {'form': form})
+
+@login_required 
+def delete_note(request, pk):
+    note = get_object_or_404(Note, id=pk)
+    if note.owner == request.user:
+        note.delete()
+    return redirect('notes:notes_list')
