4f06410ecec833ef0617f870d518f60544fe38f7
qwedrf321 <nikitakadolba@gmail.com>
2026-03-02 19:02:13 +0200

    html css and private

--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Note
 from .forms import NoteForm
+from django.db.models import Q
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -10,13 +11,27 @@ class IndexView(TemplateView):
 class NotesListView(ListView):
     model = Note
     template_name = 'notes/notes_list.html'
-    paginate_by = 3
+    paginate_by = 6
     ordering = ['-created_at']
 
-class NoteDetailView(DetailView):
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Note.objects.filter(
+                Q(is_public=True) | Q(owner=self.request.user)
+            ).order_by('-created_at')
+        else:
+            return Note.objects.filter(is_public=True).order_by('-created_at')
+
+class NoteDetailView(UserPassesTestMixin, DetailView):
     model = Note
     template_name = 'notes/note_detail.html'
     context_object_name = 'note'
+
+    def test_func(self):
+        note = self.get_object()
+        if note.is_public:
+            return True
+        return note.owner == self.request.user
 
 class NoteCreateView(LoginRequiredMixin, CreateView):
     model = Note
